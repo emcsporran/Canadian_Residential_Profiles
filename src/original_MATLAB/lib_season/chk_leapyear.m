8fc8dd8605aed0cfc8f23cdce96be89d187aad54
Eleanor McSporran <emcsporr@unb.ca>
2024-04-11 16:13:34 -0300

    Small Change to fix a function

--- src/original_MATLAB/lib_season/chk_leapyear.m
+++ src/original_MATLAB/lib_season/chk_leapyear.m
@@ -2,8 +2,8 @@ function chk_bool = chk_leapyear(year_in)
 %CHK_LEAPYEAR there is another function called leapyear that requires
 %the Aerospace toolbox but this works as well.
 
-   if (rem(year_in, 400) == 0) || (rem(year_in, 100) ~= 1) && ...
-           (rem(year_in, 4) == 0)
+   if (rem(year_in, 400) == 0) || ((rem(year_in, 100) ~= 0) && ...
+           (rem(year_in, 4) == 0))
        chk_bool = 1;
    else
        chk_bool = 0;
