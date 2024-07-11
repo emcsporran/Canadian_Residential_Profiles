b3f4ce16ec283643fd6a39373ae319c36e2fc933
EleanorMcDesktop <emcsporr@unb.ca>
2024-07-11 15:26:49 -0400

    Updates

--- src/residential_load_analysis.py
+++ src/residential_load_analysis.py
@@ -6,6 +6,7 @@ from sklearn.cluster import KMeans
 
 # Creation Info
 from src.lib.house_data import house_data
+from src.lib.time_line import time_line
 
 author_init = "ECM"
 
@@ -58,11 +59,13 @@ criteria = list()
 criteria.append("H")
 criteria.append("DHW")
 house_num = get_num(filenames, criteria)
-print("There are {house} data files in the path: \n{path}".format(house=house_num,path=path_in))
+
+print("There are {house} data files in the path: \n{path}".format(house=house_num, path=path_in))
 quick_file_check = get_num(filenames, "quick_access")
+# This will be used later to help skip processing to display previous results.
 run_old_bool = 0
 if quick_file_check > 0:
-    print("You have an existing data file with the compiled values of household consumption.\n"
+    print("\nYou have an existing data file with the compiled values of household consumption.\n"
           "Running the data compilation from scratch will take some time.\n"
           "Would you like to continue with the existing file (Y/N)?\n")
     if input() == "Y":
@@ -80,3 +83,13 @@ else:
 print(houses[1].__str__())
 print(houses[1].appliance_info())
 houses[1].data_init()
+
+# Setting up the date object.
+date_obj = time_line(path_in)
+#date_files = ["season.csv"]
+criteria = list()
+criteria.append("holiday.csv")
+print(get_num(filenames, criteria))
+if get_num(filenames, criteria) == 1:
+    date_obj.setup_holiday("holiday.csv")
+
