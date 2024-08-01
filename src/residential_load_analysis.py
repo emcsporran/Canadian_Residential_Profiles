14c3ab0b458f7ffe36d7ffd7b6dddaca1d70aa94
Eleanor McSporran <59742850+emcsporran@users.noreply.github.com>
2024-08-01 16:10:13 -0400

    Small changes and change of folder name

--- src/residential_load_analysis.py
+++ src/residential_load_analysis.py
@@ -10,12 +10,6 @@ from src.lib.time_line import time_line
 
 author_init = "ECM"
 
-# Path generation
-path = os.getcwd()
-path_lib = os.path.join(path, 'lib')
-path_in = os.path.join(path, 'input_data')
-filenames = os.listdir(path_in)
-
 
 # Functions
 def get_num(filenames, criteria):
@@ -48,19 +42,41 @@ def sort_houses(filename):
     file_temp = pd.read_csv(filename)
     house_arr = []
     for i in range(len(file_temp)):
-        path_temp = os.path.join(path_in, (file_temp.loc[i].house + ".csv"))
+        path_temp = os.path.join(path_list[1], (file_temp.loc[i].house + ".csv"))
         house_arr.append(house_data(path_temp, file_temp.loc[i]))
     return house_arr
 
 
+def chk_path(path_in):
+    for i in range(len(path_in)):
+        if not os.path.exists(path_in[i]):
+            os.makedirs(path_in[i])
+    return
+
+
+def setup_file_out(path):
+    if not os.path.isfile(path):
+        file_point = open(path, "x")
+    else:
+        file_point = open(path, "w")
+    return file_point
+
+
+# Path generation.
+path = os.getcwd()
+path_list = [os.path.join(path, 'lib'), os.path.join(path, 'input'), os.path.join(os.getcwd(), "output")] #path_lib and path_in
+chk_path(path_list)
+filenames = os.listdir(path_list[1])
+track_log = setup_file_out(os.path.join(path_list[1], "log_query_process.txt"))
+
 # Main Script
-houses = sort_houses(os.path.join(path_in, "housestat.csv"))
+houses = sort_houses(os.path.join(path_list[1], "housestat.csv"))
 criteria = list()
 criteria.append("H")
 criteria.append("DHW")
 house_num = get_num(filenames, criteria)
 
-print("There are {house} data files in the path: \n{path}".format(house=house_num, path=path_in))
+print("There are {house} data files in the path: \n{path}".format(house=house_num, path=path_list[1]))
 quick_file_check = get_num(filenames, "quick_access")
 # This will be used later to help skip processing to display previous results.
 run_old_bool = 0
@@ -85,7 +101,7 @@ print(houses[1].appliance_info())
 houses[1].data_init()
 
 # Setting up the date object.
-date_obj = time_line(path_in)
+date_obj = time_line(path_list[1])
 #date_files = ["season.csv"]
 criteria = list()
 criteria.append("holiday.csv")
